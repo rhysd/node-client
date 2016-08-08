4ee7070c0139a3637d1701777034d8d13d9d9ed7
rhysd <lin90162@yahoo.co.jp>
2016-08-08 11:24:46 +0900

    use ES2015 code for modern Node.js
    - use let/const
    - remove Promise polyfill because Node.js 4.4.5 or later supports almost
      all APIs of Promise

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /* jshint loopfunc: true, evil: true */
-var util = require('util');
-var EventEmitter = require('events').EventEmitter;
+const util = require('util');
+const EventEmitter = require('events').EventEmitter;
 
-var traverse = require('traverse');
-var Session = require('msgpack5rpc');
-var _ = require('lodash');
-var Promise = require('es6-promise').Promise;
+const Session = require('msgpack5rpc');
+const _ = require('lodash');
+const traverse = require('traverse');
 
 function Nvim(session, channel_id) {
     this._session = session;
@@ -27,18 +26,16 @@ function decode(obj) {
 }
 
 function generateWrappers(Nvim, types, metadata) {
-    for (var i = 0; i < metadata.functions.length; i++) {
-        var func = metadata.functions[i];
-        var parts = func.name.split('_');
-        var typeName = _.capitalize(parts[0]);
+    for (let i = 0; i < metadata.functions.length; i++) {
+        const func = metadata.functions[i];
+        const parts = func.name.split('_');
+        const typeName = _.capitalize(parts[0]);
         // The type name is the word before the first dash capitalized. If the type
         // is Vim, then it a editor-global method which will be attached to the Nvim
         // class.
-        var methodName = _.camelCase(parts.slice(1).join(('_')));
-        var args = _.map(func.parameters, function(param) {
-            return param[1];
-        });
-        var Type, callArgs;
+        const methodName = _.camelCase(parts.slice(1).join('_'));
+        let args = func.parameters.map(p => p[1]);
+        let Type, callArgs;
         if (typeName === 'Vim' || typeName === 'Ui') {
             Type = Nvim;
             callArgs = args.join(', ');
@@ -47,26 +44,28 @@ function generateWrappers(Nvim, types, metadata) {
             args = args.slice(1);
             // This is a method of one of the ext types, prepend "this" to the call
             // arguments.
-            callArgs = ['_this'].concat(args).join(', ');
+            callArgs = ['this'].concat(args).join(', ');
         }
         args.push('notify');
-        var params = args.join(', ');
-        var method = new Function(
+        const params = args.join(', ');
+        // XXX:
+        // Using string constructor because `notify` argument can't be distinguished
+        // if `...args` or `arguments` used.  They need if using anonymous function.
+        const method = new Function(
                 'return function ' + methodName + '(' + params + ') {' +
                 '\n  if (notify) {' +
                 '\n    this._session.notify("' + func.name + '", [' + callArgs + ']);' +
                 '\n    return;' +
                 '\n  }' +
-                '\n  var _this = this;' +
-                '\n  return new Promise(function(resolve, reject){' +
-                '\n    _this._session.request("' + func.name + '", [' + callArgs + '], function(err, res) {' +
+                '\n  return new Promise((resolve, reject) => {' +
+                '\n    this._session.request("' + func.name + '", [' + callArgs + '], (err, res) => {' +
                 '\n     if (err) return reject(new Error(err[1]));' +
-                '\n     resolve(_this._decode(res));' +
+                '\n     resolve(this._decode(res));' +
                 '\n   });' +
                 '\n  });' +
                 '\n};'
             )();
-        var paramTypes = func.parameters.map(function(p) { return p[0]; });
+        const paramTypes = func.parameters.map(p => p[0]);
         paramTypes.push('boolean');
         method.metadata = {
             name: methodName,
@@ -75,7 +74,7 @@ function generateWrappers(Nvim, types, metadata) {
             parameters: args,
             parameterTypes: paramTypes,
             canFail: func.can_fail,
-        }
+        };
         if (typeName !== 'Vim') {
             method.metadata.parameterTypes.shift();
         }
@@ -115,15 +114,14 @@ function addExtraNvimMethods(Nvim) {
 
 // Note: Use callback because it may be called more than once.
 module.exports.attach = function(writer, reader) {
-    var session = new Session([]);
-    var initSession = session;
-    var nvim = new Nvim(session)
-        var pendingRPCs = [];
-    var calledCallback = false;
+    let session = new Session([]);
+    let calledCallback = false;
+    let nvim = new Nvim(session);
+    const initSession = session;
+    const pendingRPCs = [];
 
     session.attach(writer, reader);
 
-    var _this = this;
     return new Promise(function(resolve, reject){
         // register initial RPC handlers to queue non-specs requests until api is generated
         session.on('request', function(method, args, resp) {
@@ -157,15 +155,15 @@ module.exports.attach = function(writer, reader) {
                 return reject(err);
             }
 
-            var channel_id = res[0];
+            const channel_id = res[0];
 
-            var metadata = decode(res[1]);
-            var extTypes = [];
-            var types = {};
+            const metadata = decode(res[1]);
+            const extTypes = [];
+            const types = {};
 
             Object.keys(metadata.types).forEach(function(name) {
                 // Generate a constructor function for each type in metadata.types
-                var Type = new Function(
+                const Type = new Function(
                         'return function ' + name + '(session, data, decode) { ' +
                         '\n  this._session = session;' +
                         '\n  this._data = data;' +
